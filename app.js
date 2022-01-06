64c05c04568954c9d75a764aacb6c64809c701bf
Oscar <oscar.andres.vargas1@gmail.com>
2022-01-06 06:22:35 +0100

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+// https://www.youtube.com/watch?v=cL3bXzUBFUA
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
   res.json({
-    mensaje: 'Nodejs and JWT',
+    message: 'Nodejs and JWT',
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1,
+    name: 'Oscar',
+    email: 'oscar@example',
+  };
+
+  jwt.sign({ user }, 'secretKey', (err, token) => {
+    // ? { expiresIn: '1 day' }  https://github.com/auth0/node-jsonwebtoken#readme
+    res.json({
+      token,
+    });
+  });
+});
+
+// Authorization: Bearer <token>
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretKey', (error, authData) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post has been created',
+        authData,
+      });
+    }
   });
 });
 
